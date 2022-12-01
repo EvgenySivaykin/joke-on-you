868dc1cc5faf3a13fa01bf1a6772a525888822ba
EvgenySivaykin <114056472+EvgenySivaykin@users.noreply.github.com>
2022-12-01 13:19:41 +0200

    Innicial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Valio!</h1>
+        <button>Add</button>
+        
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
