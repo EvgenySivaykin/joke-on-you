4dcb2d26bfa1a9e1233849fff7e4159ad137a1c9
EvgenySivaykin <114056472+EvgenySivaykin@users.noreply.github.com>
2022-12-02 15:43:03 +0200

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -19,26 +19,12 @@ function App() {
   }, []);
 
 
-  // useEffect(() => {
-  //   axios.get('https://v2.jokeapi.dev/joke/Programming?amount=10')
-  //   .then(res => setJokes(res.data.map(b => ({...b, show: true}))));
-  // }, []);
-
-
-
-
-
 
   return (
     <div className="App">
       <div className="App-header">
-        {/* <h1>Joke-on-you</h1> */}
+        <h1>Joke-on-you</h1>
         <ul>
-          {/* {
-            jokes?.map(j => <li key={j.id}>
-              <h3>{j.type}</h3>
-            </li>)
-          } */}
           {
             jokes?.map(j => (j.show === true) ? <Jokes key={j.id} joke={j} /> : null)
           }
