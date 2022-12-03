c6f194e0949110f5768573b27ab18ee5b79fc24c
EvgenySivaykin <114056472+EvgenySivaykin@users.noreply.github.com>
2022-12-03 18:25:17 +0200

    final commit after

--- src/Components/Jokes.jsx
+++ src/Components/Jokes.jsx
@@ -2,8 +2,8 @@ function Jokes({ joke }) {
     return (
         <li>
             <h3>{joke.joke}</h3>
-            <h3>{joke.setup}</h3>
-            <h3>{joke.delivery}</h3>
+            <h3 style={{color: 'yellow'}}>{joke.setup}</h3>
+            <h3 style={{color: 'pink'}}>{joke.delivery}</h3>
         </li>
     )
 }
